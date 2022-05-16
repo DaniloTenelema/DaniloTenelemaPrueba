949e94c01b14add026897520a4cc63e425f13588
DaniloTenelema <danilotenelema43@gmail.com>
2022-05-16 18:34:43 -0500

    commit

---
+++ app/src/main/java/com/example/danilotenelema/Activity3.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.danilotenelema.R;
+
+public class Activity3 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/danilotenelema/MainActivity.java
@@ -0,0 +1,19 @@
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.danilotenelema.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre3;
+    private EditText txtApellido3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
